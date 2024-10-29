daf72312d86bc6ecd092ff9eae81441b564a4e91
quaspar33 <kacper.ziebacz01@gmail.com>
2024-10-29 19:54:35 +0100

    main file modification

--- motors_serial/src/main.cpp
+++ motors_serial/src/main.cpp
@@ -1,5 +1,5 @@
 #include "Arduino.h"
-#include "Motor.h"
+#include "../lib/Motor/Motor.h"
 
 #define PIN_DIRECTION_FWD_1 14
 #define PIN_DIRECTION_REV_1 15
@@ -9,83 +9,35 @@
 #define PIN_DIRECTION_REV_2 17
 #define PIN_DIRECTION_PWM_2 6
 
+#define MEASUREMENT_TIME_MS 1000
+
 Motor motor1(PIN_DIRECTION_FWD_1, PIN_DIRECTION_REV_1, PIN_DIRECTION_PWM_1, 2);
 Motor motor2(PIN_DIRECTION_FWD_2, PIN_DIRECTION_REV_2, PIN_DIRECTION_PWM_2, 3);
 
-float a1 = -1.30;
-float b1 = 420.63;
-float a2 = -2.04;
-float b2 = 654.20;
-
-
-int calculatePWM(int impulses, float a, float b) {
-    int pwm = a * impulses + b;
-    return constrain(pwm, 0, 255);
-}
-
-void moveForward(int impulses) {
-    int motor1_pwm = calculatePWM(impulses, a1, b1);
-    int motor2_pwm = calculatePWM(impulses, a2, b2);
-
-    motor1.set_direction(REVERSE);
-    motor2.set_direction(FORWARD);
-    motor1.set_speed(motor1_pwm);
-    motor2.set_speed(motor2_pwm);
-
-    while (motor1.get_encoder_count() < impulses && motor2.get_encoder_count() < impulses) {
-        delay(10);
-    }
-
-    motor1.stop();
-    motor2.stop();
-    Serial.println("Move complete");
-}
-
-void turnRight(int impulses) {
-    int motor1_pwm = calculatePWM(impulses, a1, b1);
-    int motor2_pwm = calculatePWM(impulses, a2, b2);
-
-    motor1.set_direction(FORWARD);
-    motor2.set_direction(FORWARD);
-    motor1.set_speed(motor1_pwm);
-    motor2.set_speed(motor2_pwm);
-
-    while (motor1.get_encoder_count() < impulses && motor2.get_encoder_count() < impulses) {
-        delay(10);
-    }
-
-    motor1.stop();
-    motor2.stop();
-    Serial.println("Turn complete");
-}
-
-void parseCommand(String command) {
-    char cmdType = command.charAt(0);
-    int value = command.substring(2).toInt();
-    if (cmdType == 'M') {
-        moveForward(value);
-    } else if (cmdType == 'T') {
-        turnRight(value);
-    } else {
-        Serial.println("Unknown command");
-    }
-}
-
 void setup() {
     Serial.begin(9600);
+    motor1.set_encoder_count(0);
+    motor2.set_encoder_count(0);
     motor1.initialize();
     motor2.initialize();
+    motor1.set_direction(REVERSE);
+    motor2.set_direction(FORWARD);
+    motor1.set_speed(128);
+    motor2.set_speed(128);
 }
 
-//String command = "";
-
 void loop() {
+    delay(MEASUREMENT_TIME_MS);
+
+    Serial.print("PWM1: ");
+    Serial.print(analogRead(PIN_DIRECTION_PWM_1));
+    Serial.print(";PWM2: ");
+    Serial.print(analogRead(PIN_DIRECTION_PWM_2));
+    Serial.print(";Motor 1 impulses/s: ");
+    Serial.print(motor1.get_encoder_count());
+    Serial.print(";Motor 2 impulses/s: ");
+    Serial.println(motor2.get_encoder_count());
+
     motor1.reset_encoder_count();
     motor2.reset_encoder_count();
-    if (Serial.available() > 0) {
-        String command = Serial.readStringUntil('\n');
-        command.trim();
-        Serial.println(command);
-        parseCommand(command);
-    }
 }
